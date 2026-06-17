6d3965f390e2fce35a899e14352f77bfd2d17a3b
yvmp1re <zmbiez.45@gmail.com>
2026-06-17 19:40:05 +1000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -167,7 +167,7 @@ export const botConfig = {
     startingBalance: 0,
 
     // Maximum bank amount before upgrades (if upgrades are used).
-    baseBankCapacity: 100000,
+    baseBankCapacity: 1000000000,
 
     // Daily reward amount.
     dailyAmount: 100,
